d8e23fb3e73e1e4490eeee0b9711503b9dfca17c
lpoveda <lucio.poveda@3sharecorp.com>
2019-07-05 15:55:24 +0200

    saving intial changes for heroku deployment

---
+++ public/precache-manifest.48e68033af01fa6dd0f552349e8b6ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6dd5f370a6bb9b63c7efb18d42cbf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f727dd2bcbe65a097b3c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ce47b320ae1f73d4a08a",
+    "url": "/static/js/2.42c3f767.chunk.js"
+  },
+  {
+    "revision": "f727dd2bcbe65a097b3c",
+    "url": "/static/js/main.172fd15a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f751c040a2fdbdda3010475ed128f86b",
+    "url": "/static/media/spacex-logo-black-and-white.f751c040.png"
+  }
+]);
