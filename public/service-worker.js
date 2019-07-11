aa13970737cb52574d0dd1354ab77aacdca7908c
lpoveda <lucio.poveda@3sharecorp.com>
2019-07-11 14:27:12 +0200

    sending compiled public folder

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e70d990cad578fdc314e81ba63121c9.js"
+  "/precache-manifest.42190bda00031f0054a01bcfdf3cac8f.js"
 );
 
 self.addEventListener('message', (event) => {
