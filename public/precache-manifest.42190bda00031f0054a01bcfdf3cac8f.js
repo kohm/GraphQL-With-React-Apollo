39fe8095eb92c7dfc977f3a9d4fee324286878fe
lpoveda <lucio.poveda@3sharecorp.com>
2019-07-11 14:51:19 +0200

    sending compiled public folder

---
+++ public/precache-manifest.42190bda00031f0054a01bcfdf3cac8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe71c030b3d21fa12d70cc9c33dd199b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fc0f2e373de29b82901",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "15ee37d708f4d7faf244",
+    "url": "/static/js/2.1304842b.chunk.js"
+  },
+  {
+    "revision": "3fc0f2e373de29b82901",
+    "url": "/static/js/main.8e25ee6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f751c040a2fdbdda3010475ed128f86b",
+    "url": "/static/media/spacex-logo-black-and-white.f751c040.png"
+  }
+]);
